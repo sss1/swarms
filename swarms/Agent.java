ede618fa1967a3ca3b3678ddbb5352e36937b167
sss1 <sss1@andrew.cmu.edu>
2016-07-28 11:31:01 -0400

    Moved AgentComparator into Agent.java.

--- swarms/Agent.java
+++ swarms/Agent.java
@@ -1,5 +1,6 @@
 package swarms;
 
+import java.util.Comparator;
 import java.util.Random;
 import math.geom2d.Vector2D;
 
@@ -110,6 +111,15 @@ class Agent {
 
   public double getTLastUpdate() {
     return tLastUpdate;
+  }
+
+}
+
+class AgentComparator implements Comparator<Agent> {
+
+  @Override
+  public int compare(Agent a1, Agent a2) {
+    return (a1.getNextUpdateTime() < a2.getNextUpdateTime()) ? -1 : 1;
   }
 
 }
