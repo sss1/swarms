6ddc177172613772ccab429c33f9ba0c87159057
sss1 <sss1@andrew.cmu.edu>
2016-09-16 13:58:19 -0400

    Plotter now plots lines instead of shapes.

--- swarms/Plotter.java
+++ swarms/Plotter.java
@@ -9,7 +9,6 @@ import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.DatasetRenderingOrder;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYErrorRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XIntervalSeries;
 import org.jfree.data.xy.XIntervalSeriesCollection;
 import org.jfree.data.xy.XYSeries;
@@ -37,13 +36,17 @@ class Plotter extends ApplicationFrame {
   void plotMultiple(ArrayList<XIntervalSeriesCollection> allSeries, String plotFilePath) {
     final NumberAxis domainAxis = new NumberAxis("Time");
     final ValueAxis rangeAxis = new NumberAxis("Fraction of Agents");
-    final XYItemRenderer renderer0 = new XYErrorRenderer();
+    final XYErrorRenderer renderer0 = new XYErrorRenderer();
     renderer0.setSeriesStroke(0, new BasicStroke(1.0f));
+    renderer0.setSeriesLinesVisible(0, true);
+    renderer0.setSeriesShapesVisible(0, false);
     XYPlot plot = new XYPlot(allSeries.get(0), domainAxis, rangeAxis, renderer0);
     for (int i = 1; i < allSeries.size(); i++) {
       plot.setDataset(i, allSeries.get(i));
-      final XYItemRenderer renderer = new XYErrorRenderer();
+      final XYErrorRenderer renderer = new XYErrorRenderer();
       renderer.setSeriesStroke(0, new BasicStroke(1.0f));
+      renderer.setSeriesLinesVisible(0, true);
+      renderer.setSeriesShapesVisible(0, false);
       plot.setRenderer(i, renderer);
 
     }
@@ -53,6 +56,10 @@ class Plotter extends ApplicationFrame {
     domain.setTickUnit(new NumberTickUnit(10.0));
     domain.setVerticalTickLabels(true);
     domain.setRange(0.0, simDuration);
+
+    NumberAxis range = (NumberAxis) plot.getRangeAxis();
+    range.setTickUnit(new NumberTickUnit(0.1));
+    range.setRange(-0.02, 1.02);
 
     final JFreeChart chart = new JFreeChart("Fraction of Agents in Room over Time", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
     final ChartPanel panel = new ChartPanel(chart, true, true, true, true, true);
@@ -91,11 +98,13 @@ class Plotter extends ApplicationFrame {
       }
       double standardDeviation = Math.sqrt(sumSquaredDeviations / numSamples);
       double zScore95 = 1.96; // Number of standard deviations away from mean for a two-sided 95% normal confidence interval
-      double CIRadius = standardDeviation * zScore95 / Math.sqrt(numSamples);
+      double CIRadius = standardDeviation * zScore95; // Math.sqrt(numSamples);
+      double y = 1 - (i + 1) * yIncrement;
+      if (i > 0) { y = Math.min(y, averagedSeries.getYValue(i - 1)); }
       averagedSeries.add(mean,
           mean - CIRadius, // lower 95% confidence bound
           mean + CIRadius, // upper 95% confidence bound
-          1 - (i + 1) * yIncrement); // TODO: Check that this doesn't break if agents re-enter the room
+          y);
     }
     XIntervalSeriesCollection averagedSeriesAsCollection = new XIntervalSeriesCollection();
     averagedSeriesAsCollection.addSeries(averagedSeries);
